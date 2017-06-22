a976944a633224d81f2aa7d06d143d217381b1ee
Peter Johnson <pjohnson141@gmail.com>
2017-06-22 20:38:21 +0000

    Complete advanced setup

---
+++ Helps/Rails.rb
@@ -0,0 +1,9 @@
+Full command	Shortcut
+$ rails server	$ rails s
+$ rails console	$ rails c
+$ rails generate	$ rails g
+$ rails test	$ rails t
+$ bundle install	$ bundle
+
+The rails script generates a new controller with rails generate controller ControllerName <optional action names>.
+New routes are defined in the file config/routes.rb.
